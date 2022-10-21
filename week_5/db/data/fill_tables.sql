ce8a99adc9484c4b2367dbaa3b77e7df471ead50
Thao Nguyen <107896415+katrinathuthao@users.noreply.github.com>
2022-10-20 20:20:17 -0700

    Create fill_tables.sql

---
+++ week_5/db/data/fill_tables.sql
@@ -0,0 +1,9 @@
+INSERT INTO meal_rec (meal, price) VALUES ('burger', 12.42);
+INSERT INTO meal_rec (meal, price) VALUES ('ramen', 8.90);
+INSERT INTO meal_rec (meal, price) VALUES ('chicken noodle', 11.32);
+INSERT INTO meal_rec (meal, price) VALUES ('taco', 3.24);
+INSERT INTO meal_rec (meal, price) VALUES ('burrito', 10.54);
+INSERT INTO meal_rec (meal, price) VALUES ('spaghetti', 3.24);
+INSERT INTO meal_rec (meal, price) VALUES ('banana', 0.99);
+INSERT INTO meal_rec (meal, price) VALUES ('apple', 0.90);
+INSERT INTO meal_rec (meal, price) VALUES ('granola bar', 3.24);
